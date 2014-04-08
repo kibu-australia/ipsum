3ff150925d81364bf9e75e8d71670f9dd3fb1dc5
Thomas Mulvaney <mulvaney@kibu.com.au>
2014-04-08 18:36:20 +0200

    added ipsum.js

---
+++ ipsum.js
@@ -0,0 +1,12 @@
+goog.addDependency("base.js", ['goog'], []);
+goog.addDependency("../cljs/core.js", ['cljs.core'], ['goog.string', 'goog.array', 'goog.object', 'goog.string.StringBuffer']);
+goog.addDependency("../reagent/debug.js", ['reagent.debug'], ['cljs.core']);
+goog.addDependency("../clojure/string.js", ['clojure.string'], ['cljs.core', 'goog.string', 'goog.string.StringBuffer']);
+goog.addDependency("../reagent/impl/reactimport.js", ['reagent.impl.reactimport'], ['cljs.core']);
+goog.addDependency("../reagent/impl/util.js", ['reagent.impl.util'], ['cljs.core', 'reagent.debug', 'clojure.string', 'reagent.impl.reactimport']);
+goog.addDependency("../reagent/ratom.js", ['reagent.ratom'], ['cljs.core']);
+goog.addDependency("../reagent/impl/batching.js", ['reagent.impl.batching'], ['cljs.core', 'reagent.debug', 'clojure.string', 'reagent.impl.util', 'reagent.ratom']);
+goog.addDependency("../reagent/impl/component.js", ['reagent.impl.component'], ['cljs.core', 'reagent.debug', 'reagent.impl.util', 'reagent.impl.batching', 'reagent.ratom']);
+goog.addDependency("../reagent/impl/template.js", ['reagent.impl.template'], ['cljs.core', 'reagent.debug', 'clojure.string', 'reagent.impl.component', 'reagent.impl.util', 'reagent.impl.batching', 'reagent.ratom']);
+goog.addDependency("../reagent/core.js", ['reagent.core'], ['reagent.impl.template', 'cljs.core', 'reagent.impl.component', 'reagent.impl.util', 'reagent.impl.batching', 'reagent.ratom']);
+goog.addDependency("../ipsum/core.js", ['ipsum.core'], ['cljs.core', 'reagent.core']);
